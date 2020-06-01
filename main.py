6260050c06f2df9a05e24105ff4df4b934e705f8
Iarla-Boyce <65163482+Iarla-Boyce@users.noreply.github.com>
2020-06-01 11:29:57 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -16,12 +16,11 @@ for line in f:
   deltaradius.append(float(line.split('\t')[2]))
   deltavelocity.append(float(line.split('\t')[3]))
   mass.append(float(line.split('\t')[4]))
-  velocity1.append((float(G)) * (float(line.split('\t')[4])) / (float(line.split('\t')[0]))**(1/2))
+  velocity1.append((((float(G)) * (float(line.split('\t')[4]))) / (float(line.split('\t')[0])))**(1/2))
   
 x=np.array(radius)
 y=np.array(velocity)
 z=np.array(velocity1)
-
 plt.plot(x,y)
 plt.plot(x,z)
 plt.xlabel('Radius(kpc)')
